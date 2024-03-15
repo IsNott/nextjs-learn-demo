cdfd00581ac0dbf7dd54e67eb4a0a9632715df67
Nott <nott@163.com>
2024-03-15 17:51:17 +0800

    total page

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,11 +4,20 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname,useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: comment in this code when you get to this point in the course
-
+  const pathName = usePathname()
+  const searchParam = useSearchParams()
+  const currentPage = Number(searchParam?.get('page') || 1)
   // const allPages = generatePagination(currentPage, totalPages);
+
+  const createPageURL = (pageNumber: number | String) => {
+    const param = new URLSearchParams(searchParam)
+    param.set('page',pageNumber.toString())
+    return `${pathName}?${param.toString()}`;
+  }
 
   return (
     <>
